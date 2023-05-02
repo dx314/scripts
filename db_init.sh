449fc05aab83784d3e4c48884d0f306d9f2bddae
Alex Dunmow <alex@alexdunmow.com>
2023-05-02 10:29:46 +0800

    improve db gen script

--- db_init.sh
+++ db_init.sh
@@ -25,11 +25,16 @@ function start_postgresql() {
   local port=$(find_free_port)
 
   # Set the Docker container name as "$db_name"-db
-  local container_name=$(echo "${db_name}-db" | tr -c 'a-zA-Z0-9._-' '-')
+  # Check if the database name ends with "db" already
+  if [[ $db_name == *db ]]; then
+    container_name=$db_name
+  else
+    container_name=${db_name}-db
+  fi
 
   # Start the PostgreSQL container with the given password, port, and database name
   docker run -d --name "$container_name" -e POSTGRES_PASSWORD="$db_password" -e POSTGRES_USERNAME="db_user" -e POSTGRES_DB="$db_name" -p "$port":5432 postgres:latest
-
+  local uri="postgresql://$db_user:$db_password@localhost:$port/$db_name"
   # Save the connection details to an environment file
   cat >.db.env <<EOF
 DB_NAME=$db_name
@@ -37,12 +42,11 @@ DB_USER=$db_name
 DB_PASSWORD=$db_password
 DB_HOST=localhost
 DB_PORT=$port
+DB_URI=$uri
 EOF
 
   echo "Started PostgreSQL db container, $db_name, with password $db_password on port $port"
   echo "Created .db.env file with connection details"
-
-  local uri="postgresql://$db_user:$db_password@localhost:$port/$db_name"
   echo "PostgreSQL URI: $uri"
   local psql_cmd="psql -h localhost -p $port -U $db_name -W $db_name"
   echo "Run the following command to connect to the PostgreSQL database:"
