a533d9101bdc707a93d0874fc3e4204ce661f79e
dx314 <126582081+dx314@users.noreply.github.com>
2023-08-28 16:47:09 +0800

    Create get_mailbox_memberships.ps1

---
+++ powershell/get_mailbox_memberships.ps1
@@ -0,0 +1,27 @@
+# Import the Exchange Online PowerShell Module
+Import-Module ExchangeOnlineManagement
+
+# Connect to Exchange Online PowerShell
+Connect-ExchangeOnline -UserPrincipalName alex.dunmow.admin@millcrest.com.au
+
+# Get all shared mailboxes
+$SharedMailboxes = Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited
+
+# Display all shared mailboxes and allow selection
+$SelectedMailboxes = $SharedMailboxes | Out-GridView -Title "Select Shared Mailboxes" -PassThru
+
+# Get all users
+$AllUsers = Get-Mailbox -ResultSize Unlimited
+
+# Display all users and allow selection
+$SelectedUsers = $AllUsers | Out-GridView -Title "Select Users" -PassThru
+
+# Add selected users to selected mailboxes
+foreach ($mailbox in $SelectedMailboxes) {
+    foreach ($user in $SelectedUsers) {
+        Add-MailboxPermission -Identity $mailbox.DistinguishedName -User $user.UserPrincipalName -AccessRights FullAccess -InheritanceType All
+    }
+}
+
+# Disconnect the Exchange Online PowerShell session
+Disconnect-ExchangeOnline -Confirm:$false
